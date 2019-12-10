217d8ab5184d777c30dc5ee534b9008979653625
Luz Clarita <luzpm@uamv.edu.ni>
2019-12-10 15:56:13 -0600

    dispose on close en nuevo Ticket

--- src/GUI/tickets/registrarNuevoT.java
+++ src/GUI/tickets/registrarNuevoT.java
@@ -45,7 +45,7 @@ public class registrarNuevoT extends javax.swing.JFrame {
         selec = new javax.swing.JButton();
         cancel = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Montserrat", 0, 18)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
